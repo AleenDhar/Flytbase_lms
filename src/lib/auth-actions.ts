cc884780670f0d429e7b3be7c2c70ece2d901124
Aleen Dhar <aleendhar23@gmail.com>
2025-03-19 16:32:00 +0530

    is this okay mf

--- src/lib/auth-actions.ts
+++ src/lib/auth-actions.ts
@@ -7,9 +7,8 @@ import { createClient } from "@/utils/supabase/server";
 
 const getURL = () => {
   let url =
-    process.env.NEXT_PUBLIC_SITE_URL ??
-    process.env.NEXT_PUBLIC_VERCEL_URL ??
-    "http://localhost:3000";
+    process.env.NEXT_PUBLIC_SITE_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL;
+  // "http://localhost:3000";
 
   url = url.startsWith("http") ? url : `https://${url}`;
 
