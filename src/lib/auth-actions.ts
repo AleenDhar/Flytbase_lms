b48130818465b959b6cd1473d068ab3a276bcd5c
Aleen Dhar <aleendhar23@gmail.com>
2025-03-19 16:11:47 +0530

    is this okay mf

--- src/lib/auth-actions.ts
+++ src/lib/auth-actions.ts
@@ -4,17 +4,19 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 import { createClient } from "@/utils/supabase/server";
+
 const getURL = () => {
   let url =
     process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    process?.env?.NEXT_PUBLIC_VERCEL_URL; // Automatically set by Vercel.
-  // ?? "http://localhost:3000/";
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    "http://localhost:3000/";
   // Make sure to include `https://` when not localhost.
   url = url.startsWith("http") ? url : `https://${url}`;
   // Make sure to include a trailing `/`.
   url = url.endsWith("/") ? url : `${url}/`;
   return url;
 };
+
 export async function login(formData: FormData) {
   const supabase = await createClient();
 
