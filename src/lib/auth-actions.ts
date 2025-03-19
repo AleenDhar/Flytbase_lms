7093443fdb28bbec46adbb8ad21eeb9ef8bd7913
Aleen Dhar <aleendhar23@gmail.com>
2025-03-19 16:04:40 +0530

    is this okay mf

--- src/lib/auth-actions.ts
+++ src/lib/auth-actions.ts
@@ -4,7 +4,17 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 import { createClient } from "@/utils/supabase/server";
-
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    "http://localhost:3000/";
+  // Make sure to include `https://` when not localhost.
+  url = url.startsWith("http") ? url : `https://${url}`;
+  // Make sure to include a trailing `/`.
+  url = url.endsWith("/") ? url : `${url}/`;
+  return url;
+};
 export async function login(formData: FormData) {
   const supabase = await createClient();
 
@@ -69,6 +79,7 @@ export async function signInWithGoogle() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
+      redirectTo: getURL(),
       queryParams: {
         access_type: "offline",
         prompt: "consent",
